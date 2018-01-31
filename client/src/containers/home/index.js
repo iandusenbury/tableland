b2ead84acbec562f7f7e406cfc024adbf4aaf96e
djarredon <djarredon@gmail.com>
2018-01-30 17:24:36 -0800

    added home/index.js back in

---
+++ client/src/containers/home/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { push } from 'react-router-redux'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
+
+const Home = props => {
+  const { changePage, message } = props
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>{message}</p>
+
+      <button onClick={() => changePage()}>Go to about page via redux</button>
+    </div>
+  )
+}
+
+const mapStateToProps = state => {
+  return {
+    //message: state.app.example.message
+    message: state.navbar
+  }
+}
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+  changePage: () => push('/about-us')
+}, dispatch)
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Home)

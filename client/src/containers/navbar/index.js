46d920a3a95d10d3185c344bfde77f5b3299ea56
djarredon <djarredon@gmail.com>
2018-01-31 10:36:03 -0800

    more space work

--- client/src/containers/navbar/index.js
+++ client/src/containers/navbar/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Link } from 'react-router-dom'
 import About from '../about'
-//	material-ui
+//  material-ui
 import IconMenu from 'material-ui/IconMenu'
 import IconButton from 'material-ui/IconButton'
 import MenuItem from 'material-ui/MenuItem'
@@ -17,64 +17,64 @@ import NavigationExpandMoreIcon from 'material-ui/svg-icons/navigation/menu'
 
 // layout should be
 /*
-	 M(icon/link)	[Search Bar] [Submit Button] [Hamburger Menu]
+   M(icon/link) [Search Bar] [Submit Button] [Hamburger Menu]
 
-	 [Hamburger Menu] (if not signed in)
-	 "Sign In"
-	 "About"
+   [Hamburger Menu] (if not signed in)
+   "Sign In"
+   "About"
 
-	 [Hamburger Menu] (if signed in)
-	 "View Profile"
-	 "Edit Profile"
-	 "View Admin"	(if admin)
-	 "Sign Out"
-	 "About"
+   [Hamburger Menu] (if signed in)
+   "View Profile"
+   "Edit Profile"
+   "View Admin" (if admin)
+   "Sign Out"
+   "About"
 */
 export default () => (
-	<header>
-		<Toolbar>
-			<ToolbarGroup>
-				<FlatButton 
-					containerElement={<Link to="/" />}
-					label="MESA" />
+  <header>
+    <Toolbar>
+      <ToolbarGroup>
+        <FlatButton 
+          containerElement={<Link to="/" />}
+          label="MESA" />
 
-			</ToolbarGroup>
-			<ToolbarGroup>
-				<TextField hintText="Search" />
-				<RaisedButton label="Search" />
-				<IconMenu iconButtonElement={
-						<IconButton touch={true}>
-							<NavigationExpandMoreIcon />
-						</IconButton>
-				}>
-					<MenuItem 
-						containerElement={<Link to="/" />}
-						value="sign_in" 
-						primaryText="Sign In" />
-					<Divider />
-					<MenuItem 
-						containerElement={<Link to="/" />}
-						value="view_profile" 
-						primaryText="View Profile" />
-					<MenuItem 
-						containerElement={<Link to="/" />}
-						value="edit_profile" 
-						primaryText="Edit Profile" />
-					<MenuItem 
-						containerElement={<Link to="/" />}
-						value="admin_page" 
-						primaryText="Admin Page" />
-					<Divider />
-					<MenuItem 
-						containerElement={<Link to="/" />}
-						value="sign_out" 
-						primaryText="Sign Out" />
-					<MenuItem 
-						containerElement={<Link to="/about-us" />}
-						value="about" 
-						primaryText="About" />
-				</IconMenu>
-			</ToolbarGroup>
-		</Toolbar>
-	</header>
+      </ToolbarGroup>
+      <ToolbarGroup>
+        <TextField hintText="Search" />
+        <RaisedButton label="Search" />
+        <IconMenu iconButtonElement={
+            <IconButton touch={true}>
+              <NavigationExpandMoreIcon />
+            </IconButton>
+        }>
+          <MenuItem 
+            containerElement={<Link to="/" />}
+            value="sign_in" 
+            primaryText="Sign In" />
+          <Divider />
+          <MenuItem 
+            containerElement={<Link to="/" />}
+            value="view_profile" 
+            primaryText="View Profile" />
+          <MenuItem 
+            containerElement={<Link to="/" />}
+            value="edit_profile" 
+            primaryText="Edit Profile" />
+          <MenuItem 
+            containerElement={<Link to="/" />}
+            value="admin_page" 
+            primaryText="Admin Page" />
+          <Divider />
+          <MenuItem 
+            containerElement={<Link to="/" />}
+            value="sign_out" 
+            primaryText="Sign Out" />
+          <MenuItem 
+            containerElement={<Link to="/about-us" />}
+            value="about" 
+            primaryText="About" />
+        </IconMenu>
+      </ToolbarGroup>
+    </Toolbar>
+  </header>
 )

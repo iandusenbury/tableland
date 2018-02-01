67596b0cdfde3f368abd03c02a207eef27cd0430
Matt <matthewballeza@gmail.com>
2018-01-31 21:08:04 -0800

    [#63] Replaced RT with MUI components. Edited code style.

--- client/src/containers/admin/index.js
+++ client/src/containers/admin/index.js
@@ -1,37 +1,21 @@
 import React from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import Input from 'react-toolbox/lib/input/Input'
-import { Table, TableHead, TableRow, TableCell } from 'react-toolbox/lib/table'
-import Button from 'react-toolbox/lib/button/Button'
 import Users from './users'
 import { adminChangeTableTo, adminChangeAdminTo } from '../../actions'
 
-import Paper from 'material-ui/Paper'
-import Menu from 'material-ui/Menu'
-import MenuItem from 'material-ui/MenuItem'
-require('./style.css')
+import {
+  Paper,
+  Menu,
+  MenuItem,
+} from 'material-ui'
 
-/*
-    Purpose: Allow user/admin access to the list of profiles in the database.
-
-    Features:
-    - Menu Bar. The user can select which list to view, the new users, remove
-    users, and elevate users.
-    - Search Bar. The user can filter through the list if needed based on
-    TODO(first and last name, id, ...?).
-      - The list will be updated upon...?
-        - Enter
-      - The search text will be cleared after Selection or Enter
-    - Table List. Each entry will display identifying information about a
-    profile. Each row will have buttons:
-      - View profile in new tab
-      - Confirm a positive action (ex: "Allow")
-      - Confirm a negative action (ex: "Deny")
-*/
+import './style.css'
 
 const AdminPage = props => {
   let page
+  let menu
+
   const {
     tables,
     currentTable,
@@ -43,13 +27,6 @@ const AdminPage = props => {
   if(isAdmin) {
     page = (
       <div className='table-section'>
-        <Paper zDepth={0} className='menu-bar'>
-          <Menu>
-            <MenuItem primaryText={tables[0].title} onClick={() => adminChangeTableTo(0)}/>
-            <MenuItem primaryText={tables[1].title} onClick={() => adminChangeTableTo(1)}/>
-            <MenuItem primaryText={tables[2].title} onClick={() => adminChangeTableTo(2)}/>
-          </Menu>
-        </Paper>
         <Users
           title={currentTable.title}
           users={currentTable.list}
@@ -58,7 +35,22 @@ const AdminPage = props => {
         />
       </div>
     )
+    menu = (
+      <Menu>
+        {
+          tables.map((table) => {
+            return (
+              <MenuItem
+                primaryText={table.title}
+                onClick={() => adminChangeTableTo(table.id)}
+              />
+            )
+          })
+        }
+      </Menu>
+    )
   } else {
+    adminChangeTableTo(2) // Hack, for now
     page = (
       <Users
         title={currentTable.title}
@@ -72,7 +64,12 @@ const AdminPage = props => {
   return (
     <div>
       <h1>Admin Page</h1>
-      {page}
+      <div className='menu-bar'>
+        <Paper zDepth={0}>
+          { menu }
+        </Paper>
+      </div>
+      { page }
     </div>
   )
 }

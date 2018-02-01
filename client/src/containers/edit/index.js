6a95d22a100897b006b2f6b41d03d07dcb48d73d
Halala <khalala@pdx.edu>
2018-01-31 16:18:19 -0800

    rough draft

---
+++ client/src/containers/edit/index.js
@@ -0,0 +1,16 @@
+/* This file is the main edit page, which is
+* the page that users will see as soon as they click the
+* edit page from the menu*/
+import React from 'react'
+import { Component } from 'react'
+
+class MainEditPage extends Component {
+
+    render() {
+        <div></div>
+    }
+
+
+}
+
+export default MainEditPage;

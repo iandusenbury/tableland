ce63f7fb18476fcb7b53ee7d46902fe853dfb2d2
djarredon <djarredon@gmail.com>
2018-01-31 17:24:03 -0800

    Added home/index.js to commit, removed depracated comments from map/index.js

--- client/src/containers/home/index.js
+++ client/src/containers/home/index.js
@@ -2,15 +2,14 @@ import React from 'react'
 import { push } from 'react-router-redux'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
+import Map from '../map'
 
 const Home = props => {
   const { changePage, message } = props
 
   return (
     <div>
-      <h1>Home</h1>
-      <p>{message}</p>
-      <button onClick={() => changePage()}>Go to about page via redux</button>
+      <Map />
     </div>
   )
 }

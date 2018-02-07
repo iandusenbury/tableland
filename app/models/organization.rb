211de8a80c7b9768f57d71f410ccdd8de2527922
Evan White <evwhite2993@gmail.com>
2018-02-06 22:36:55 -0800

    [#112] Setup of associations and validations for the Permission model to fit in with the rest of the models (schema).

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,9 +2,14 @@ class Organization < ApplicationRecord
   # Associations
   has_many :experiences, dependent: :destroy
   has_many :users, through: :experiences
+  
   has_many :sponsors, dependent: :destroy
   has_many :programs, through: :sponsors
+  
   has_many :media, as: :mediable, dependent: :destroy
+  
+  has_many :permissions, dependent: :destroy
+  has_many :admins, through: :permissions, source: :user
 
   # Validations
   validates :name, :visible, :address_line_1, :address_line_2, :city, :country, presence: true
--- app/models/permission.rb
+++ app/models/permission.rb
@@ -1,2 +1,10 @@
 class Permission < ApplicationRecord
+  # Associations
+  belongs_to :user
+  belongs_to :organization, optional: true
+  belongs_to :program, optional: true
+  
+  # Validations
+  validates :user_id, presence: true
+  validates :program_id, absence: true, if: :organization_id?
 end

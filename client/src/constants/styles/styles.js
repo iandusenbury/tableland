e97577bb64208db846d8f89a8448c0c68f9c24d5
djarredon <djarredon@gmail.com>
2018-02-08 15:38:59 -0800

    commented out unused MESA colors

--- client/src/constants/styles/styles.js
+++ client/src/constants/styles/styles.js
@@ -1,9 +1,11 @@
-const quantumOrange = "#ff9e15"
 const thermalRed = "#ea4e46"
+/*
+const quantumOrange = "#ff9e15"
 const atomicGreen = "#bed62f"
 const waveBlue = "#8195b1"
 const rawUmber = "#715558"
 const sand = "#e7e0d7"
+*/
 
 
 export const style = {

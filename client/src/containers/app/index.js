75e59f4ff31d3604f1dcc3d518887f9f9bd757b9
djarredon <djarredon@gmail.com>
2018-02-09 15:08:15 -0800

    Removed muiTheme from src/index.js and removed commented out section from containers/app/index.js

--- client/src/containers/app/index.js
+++ client/src/containers/app/index.js
@@ -1,13 +1,3 @@
-/*  created linting errors
-import React, { Component } from 'react'
-import { Route } from 'react-router-dom'
-import Home from '../../components/home'
-import About from '../../components/about'
-import Navbar from '../../components/navbar'
-
-
-import { bindActionCreators } from 'redux'
-*/
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 
--- client/src/index.js
+++ client/src/index.js
@@ -2,32 +2,21 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'react-router-redux'
-import { theme, MuiThemeProvider } from 'material-ui/styles'
 
 import store, { history } from './base/store'
 import App from './containers/app'
 
 // import material-ui theme
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import './constants/styles/styles.js'
 
 const target = document.querySelector('#root')
-
-const muiTheme = theme({
-  palette: {
-    backgroundColor:  '#ffc0cb',
-    quantumOrange:    '#ff9e15',
-    thermalRed:       '#ea4e46',
-    atomicGreen:      '#bed62f',
-    waveBlue:         '#8195b1',
-    rawUmber:         '#715558',
-    sand:             '#e7e0d7'
-  }
-});
 
 render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
       <div>
-        <MuiThemeProvider muiTheme={muiTheme}>
+        <MuiThemeProvider>
           <App />
         </MuiThemeProvider>
       </div>

3bf3b756875b24aed4de75f79ecc62b6510ce031
Ian Dusenbury <ian.dusenbury@gmail.com>
2018-02-09 18:27:19 -0800

    [#99] fixed authentication errors and exception handling

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :experiences
   resources :media
   resources :programs
   resources :organizations
   resources :users
   resources :sessions, only: [:create, :destroy]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :users, skip: [:sessions, :registrations, :passwords]
 
   namespace :api do
     namespace :v1 do

ef8a331a27bac938737bea1d8014485b462a80f1
Matt <matthewballeza@gmail.com>
2018-02-12 17:32:13 -0800

    Branch from Trello tag 116. Added tabs.

---
+++ client/src/constants/tabs/tabViewMap.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+
+export default () => (
+  <div className="wholeTabTop">
+    <div className="topTab" />
+    <div className="stubTabTop">
+      <p>View Map</p>
+    </div>
+    <div className="arrowTop" />
+  </div>
+)
---
+++ client/src/constants/tabs/tabViewProfile.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+
+export default () => (
+  <div className="wholeTabBottom">
+    <div className="stubTabBottom">
+      <p>View Profile</p>
+    </div>
+    <div className="bottomTab" />
+    <div className="arrowBottom" />
+  </div>
+)

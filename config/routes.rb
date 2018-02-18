f73c3efbdf975df7e8400e9f31cbf7c04112bf96
Evan White <evwhite2993@gmail.com>
2018-02-18 01:39:47 -0800

    [#124] Add constraint for routing to only work within the api.roadmaps subdomain

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  scope module: 'api' do
-    namespace :v1 do
-      # Mock up for testing search results serialization
-      get '/search', to: 'api_base#search'
+  constraints subdomain: 'api.roadmaps' do
+    scope module: 'api' do
+      namespace :v1 do
+        # Mock up for testing search results serialization
+        get '/search', to: 'api_base#search'
 
-      resources :users
-      resources :experiences
-      resources :media
-      resources :programs
-      resources :organizations
+        resources :users
+        resources :experiences
+        resources :media
+        resources :programs
+        resources :organizations
+      end
     end
   end
 end

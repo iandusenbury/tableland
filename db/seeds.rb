2b0d256f679956c8c0705d646d99f6581f358ae5
Dan Crayne <dan.crayne@gmail.com>
2018-02-18 18:17:36 -0800

    Add record generation for remaining relationship tables and reformat

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,48 +5,52 @@ NUM_USERS = 50
 NUM_ORGS  = 50
 NUM_PROGS = 20
 
+
 def create_users(num_users)
   for i in 1..num_users do
     User.create( 
-                first_name:   Faker::Name.first_name, 
-                last_name:    Faker::Name.last_name,
-                linkedin_id:  Faker::Lorem.characters(10),
-                contact_url:  Faker::Internet.url,
-                visible:      Faker::Boolean.boolean(0.8),
-                role:         :user,
-                description:  Faker::Lorem.paragraph
+      first_name:   Faker::Name.first_name, 
+      last_name:    Faker::Name.last_name,
+      linkedin_id:  Faker::Lorem.characters(10),
+      contact_url:  Faker::Internet.url,
+      visible:      Faker::Boolean.boolean(0.8),
+      role:         :user,
+      description:  Faker::Lorem.paragraph
     )
   end
 end
+
 
 def create_organizations(num_orgs)
   for i in 1..num_orgs do
     Organization.create( 
-                        name:           Faker::Company.name, 
-                        description:    Faker::Lorem.paragraph,
-                        url:            Faker::Internet.url,
-                        visible:        Faker::Boolean.boolean(0.8),
-                        category:       ["institution", "company", "organization"].sample,
-                        address_line_1: Faker::Address.street_address,
-                        address_line_2: Faker::Address.secondary_address,
-                        city:           Faker::Address.city,
-                        state:          Faker::Address.state_abbr,
-                        postal_code:    Faker::Address.postcode,
-                        country:        Faker::Address.country
+      name:           Faker::Company.name, 
+      description:    Faker::Lorem.paragraph,
+      url:            Faker::Internet.url,
+      visible:        Faker::Boolean.boolean(0.8),
+      category:       ["institution", "company", "organization"].sample,
+      address_line_1: Faker::Address.street_address,
+      address_line_2: Faker::Address.secondary_address,
+      city:           Faker::Address.city,
+      state:          Faker::Address.state_abbr,
+      postal_code:    Faker::Address.postcode,
+      country:        Faker::Address.country
     )
   end
 end
 
+
 def create_programs(num_programs)
   for i in 1..num_programs do
     Program.create( 
-                   name:        "#{Faker::Lorem.word.capitalize} Club",
-                   description: Faker::Lorem.paragraph,
-                   url:         Faker::Internet.url,
-                   visible:     Faker::Boolean.boolean(0.8)
+      name:        "#{Faker::Lorem.word.capitalize} Club",
+      description: Faker::Lorem.paragraph,
+      url:         Faker::Internet.url,
+      visible:     Faker::Boolean.boolean(0.8)
     )
   end
 end
+
 
 def create_experiences()
   org_recs_per_user =  { :MIN => 1, :MAX => 5 }
@@ -61,24 +65,24 @@ def create_experiences()
       if j == 0
         # this is the most recent experience
         Experience.create( 
-                          user_id:         i,
-                          organization_id: Faker::Number.between(1, NUM_ORGS),
-                          start_date:      1.year.ago,
-                          end_date:        nil,
-                          title:           Faker::Company.profession,
-                          award:           Faker::Educator.course,
-                          primary:         true
-                         )
+          user_id:         i,
+          organization_id: Faker::Number.between(1, NUM_ORGS),
+          start_date:      1.year.ago,
+          end_date:        nil,
+          title:           Faker::Company.profession,
+          award:           Faker::Educator.course,
+          primary:         true
+         )
       else
         Experience.create( 
-                          user_id:         i,
-                          organization_id: Faker::Number.between(1, NUM_ORGS),
-                          start_date:      (j * 2).years.ago,
-                          end_date:        j.years.ago,
-                          title:           Faker::Company.profession,
-                          award:           Faker::Educator.course,
-                          primary:         false
-                         )
+          user_id:         i,
+          organization_id: Faker::Number.between(1, NUM_ORGS),
+          start_date:      (j * 2).years.ago,
+          end_date:        j.years.ago,
+          title:           Faker::Company.profession,
+          award:           Faker::Educator.course,
+          primary:         false
+         )
       end
     end
 
@@ -88,18 +92,90 @@ def create_experiences()
     
     for j in 0..(max_recs - min_recs) do
       Experience.create( 
-                        user_id:         i,
-                        program_id:      Faker::Number.between(1, NUM_PROGS),
-                        start_date:      (j*2).years.ago,
-                        end_date:        j.years.ago,
-                        title:           "Member",
-                        primary:         false
+        user_id:         i,
+        program_id:      Faker::Number.between(1, NUM_PROGS),
+        start_date:      (j*2).years.ago,
+        end_date:        j.years.ago,
+        title:           "Member",
+        primary:         false
       )
     end
   end
 end
 
+
+def create_sponsors()
+  for i in 1..NUM_PROGS do
+    Sponsor.create(
+      organization_id: Faker::Number.between(1, NUM_ORGS),
+      program_id:      i
+    )
+  end
+end
+
+
+def create_permissions() 
+  for i in 1..NUM_ORGS do
+    Permission.create(
+      user_id:         Faker::Number.between(1, NUM_USERS),
+      organization_id: i
+    )
+  end
+
+  for i in 1..NUM_PROGS do
+    Permission.create(
+      user_id:    Faker::Number.between(1, NUM_USERS),
+      program_id: i
+    )
+  end
+end
+
+
+def create_media()
+  # every user and organization get both an image and video
+  for i in 1..NUM_USERS do
+    Medium.create(
+      mediable_id:    i,
+      mediable_type:  "User",
+      category:       "image",
+      description:    Faker::Lorem.sentence,
+      url:            Faker::LoremPixel.image
+    )
+    
+    Medium.create(
+      mediable_id:    i,
+      mediable_type:  "User",
+      category:       "video",
+      description:    Faker::Lorem.sentence,
+      url:            "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    )
+  end
+
+
+  for i in 1..NUM_ORGS do
+    Medium.create(
+      mediable_id:    i,
+      mediable_type:  "Organization",
+      category:       "image",
+      description:    Faker::Lorem.sentence,
+      url:            Faker::LoremPixel.image
+    )
+
+    Medium.create(
+      mediable_id:    i,
+      mediable_type:  "Organization",
+      category:       "video",
+      description:    Faker::Lorem.sentence,
+      url:            "https://www.youtube.com/watch?v=ksBE53CIT8E"
+    )
+  end
+end
+
+
 create_users(NUM_USERS)
 create_organizations(NUM_ORGS)
 create_programs(NUM_PROGS)
 create_experiences()
+create_sponsors()
+create_media()
+create_permissions()

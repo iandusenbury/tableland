6e25b12d51343ea6e87ee1aba454a8458633a517
Evan White <evwhite2993@gmail.com>
2018-02-19 15:22:00 -0800

    [#124] Configure appropriate routes for our resources - Include an example setup that uses a ‘/admin’ scope to separate out actions that can only be performed by admins. - Include a commented out example setup that does not separate out admin actions.

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,95 @@ Rails.application.routes.draw do
     scope module: 'api' do
       namespace :v1 do
         # Mock up for testing search results serialization
-        get '/search', to: 'api_base#search'
+        get 'search', to: 'api_base#search'
 
-        resources :users
-        resources :experiences
-        resources :media
-        resources :programs
-        resources :organizations
+        # resources :users
+        # resources :experiences
+        # resources :media
+        # resources :programs
+        # resources :organizations
+
+        # ------------ With admin scope separation -------------------------
+        # Get a specific user with all their media and experiences, update a specific user, destroy a specific user
+        resources :users, only: [:show, :update, :destroy] do
+          # Get a random user
+          collection do
+            get 'random'
+          end
+          # Create experiences for a user, update experiences for a user, destory experiences for a user
+          resources :experiences, only: [:create, :update, :destroy]
+          # Create media for a user, update media for a user, destroy media for a user
+          resources :media, only: [:create, :update, :destroy]
+        end
+
+        # Get a specific organization with all their media, users, and programs, create a new organization
+        resources :organizations, only: [:show, :create] do
+          # Create programs for an organization
+          resources :programs, only: :create
+        end
+
+        # Get a specific program with all their media, users, and parent organizations
+        resources :programs, only: :show
+
+        scope '/admin' do
+          # Super admins can get all users
+          resources :users, only: :index do
+            # Admins can get all their permissions, super admins can grant new permissions 
+            member do
+              get 'permissions',  to: 'users#permissions'
+              post 'permissions', to: 'users#add_permission'
+            end
+          end
+
+          # Super admins can see all orgs (optional), admins can update orgs, admins can destroy orgs
+          resources :organizations, only: [:index, :update, :destroy] do
+            # Admins can create media for orgs, update media for orgs, and destroy media for orgs
+            resources :media, only: [:create, :update, :destroy]
+          end
+
+          # Super admins can see all progs (optional), admins can update progs, admins can destroy progs
+          resources :programs, only: [:index, :update, :destroy] do
+            # Admins can create media for progs, update media for progs, and destroy media for progs
+            resources :media, only: [:create, :update, :destroy]
+          end
+        end
+        # ------------------------------------------------------------------
+
+        # ------------ Without admin scope separation ----------------------
+        # Get all users (super_admin), Get a specific user with all their media and experiences, update a specific user, destroy a specific user
+        # resources :users, only: [:index, :show, :update, :destroy] do
+        #   # Get a random user
+        #   collection do
+        #     get 'random'
+        #   end
+        #   # Get all permissions (admins), Assign a new permission (super_admin)
+        #   member do
+        #       get 'permissions',  to: 'users#permissions'
+        #       post 'permissions', to: 'users#add_permission'
+        #   end
+        #   # Create experiences for a user, update experiences for a user, destory experiences for a user
+        #   resources :experiences, only: [:create, :update, :destroy]
+        #   # Create media for a user, update media for a user, destroy media for a user
+        #   resources :media, only: [:create, :update, :destroy]
+        # end
+
+        # # Get all orgs (super_admin/optional), Get a specific organization with all their media, users, and programs, create a new organization,
+        # # Update an org (admins), destroy an org (admins)
+        # resources :organizations do
+        #   # Create programs for an organization
+        #   resources :programs, only: :create
+        #   # Create media for orgs (admins), update media for orgs (admins), and destroy media for orgs (admins)
+        #   resources :media, only: [:create, :update, :destroy]
+        # end
+
+        # # Get all progs (super_admin/optional), Get a specific program with all their media, users, and parent organizations,
+        # # Update a prog (admins), destroy a prog (admins)
+        # resources :programs, only: [:index, :show, :update, :destroy] do
+        #     # Create media for progs (admins), update media for progs (admins), and destroy media for progs (admins)
+        #     resources :media, only: [:create, :update, :destroy]
+        # end
+        # ------------------------------------------------------------------
+
       end
     end
   end

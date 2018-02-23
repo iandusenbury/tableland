f935019ea3f521cb8fe7c0c906eae475e199e755
Dan Crayne <dan.crayne@gmail.com>
2018-02-23 07:41:34 -0800

    add simple pattern search
    This simple search returns a json result based on a pattern match of a
    search term given as a query string parameter:
    e.g. http://localhost:5000/search?term='club'
    
    It still needs the results to be added to a single 'results' root,
    instead of each record type (user,org,prog) receiving their own 'result'
    root.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def self.fields_to_query(field_list)
+    query = ""
+    query = fields_to_search.join(" LIKE :term ")
+    query += " LIKE :term "
+  end
 end
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,4 +1,15 @@
 class Organization < ApplicationRecord
+
+  def self.search(term)
+    fields_to_search = ['name', 'description', 'url', 'city']
+    clause = ApplicationController.build_like_clause(fields_to_search)
+
+    search_results = []
+    search_results << Organization.where(clause, term: "%#{term}%")
+
+    return search_results
+  end
+
   # Associations
   has_many :experiences, dependent: :destroy
   has_many :users, through: :experiences
@@ -14,4 +25,5 @@ class Organization < ApplicationRecord
   # Validations
   validates :name, :address_line_1, :address_line_2, :city, :country, presence: true
   validates :visible, inclusion: { in: [true, false] }
+
 end

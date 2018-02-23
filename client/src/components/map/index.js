c194890aa72201c3c2141693e25a88a5ecbdc160
Matt <matthewballeza@gmail.com>
2018-02-22 19:56:45 -0800

    Merge playground/mballeza to playground/roadmap_build.

--- client/src/components/map/index.js
+++ client/src/components/map/index.js
@@ -1,10 +1,6 @@
-import React from 'react'
-import { compose, withProps } from 'recompose'
-import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap
-} from 'react-google-maps'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { withScriptjs, withGoogleMap, GoogleMap } from 'react-google-maps'
 import mapStyle from './style.json'
 
 /*
@@ -12,28 +8,73 @@ import mapStyle from './style.json'
  *    Improve with more specific styling
 */
 
-/* eslint-disable no-unused-vars */
-const MyMapComponent = compose(
-  withProps({
-    googleMapURL:
-      'https://maps.googleapis.com/maps/api/js?key=AIzaSyARRsWk_FbczyZ0RFU4STmiTxxYfnWmiBs&v=3.exp&libraries=geometry,drawing,places',
-      loadingElement: <div style={{ height: `100%` }} />,
-      containerElement: <div style={{ height: 'calc(100vh - 72px)' }} />,
-      mapElement: <div style={{ height: `100%` }} />
-  }),
-  withScriptjs,
-  withGoogleMap
-)(props => (
-  <GoogleMap
-    defaultZoom={12}
-    defaultCenter={{ lat: 45.516, lng: -122.679565 }}
-    defaultOptions={{
-      styles: mapStyle,
-      streetViewControl: false,
-      mapTypeControl: false,
-      fullscreenControl: false
-    }}
-  />
-))
+const GMap = withScriptjs(
+  withGoogleMap(props => {
+    const { onMapMounted, markers, polylines, ...otherProps } = props
+    return (
+      <GoogleMap
+        {...otherProps}
+        ref={c => {
+          onMapMounted(c)
+        }}>
+        {markers.map(marker => marker)}
+        {polylines.map(poly => poly.children.map(child => child))}
+        {polylines.map(poly => poly.nextParent)}
+      </GoogleMap>
+    )
+  })
+)
+
+class MyMapComponent extends Component {
+  constructor(props) {
+    super(props)
+
+    this.handleMapMounted = this.handleMapMounted.bind(this)
+  }
+  componentWillMount() {
+    const { buildRoadMap } = this.props
+    buildRoadMap()
+  }
+
+  // When map is mounted, extend the bounds of the map view to fit all markers
+  handleMapMounted(c) {
+    let { mapRef } = this.props
+    const { bounds } = this.props
+    mapRef = c
+    mapRef.fitBounds(bounds)
+  }
+
+  render() {
+    const { markers, polylines } = this.props
+
+    return (
+      <GMap
+        markers={markers}
+        polylines={polylines}
+        onMapMounted={this.handleMapMounted}
+        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyARRsWk_FbczyZ0RFU4STmiTxxYfnWmiBs&v=3.exp&libraries=geometry,drawing,places"
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `calc(100vh - 72px)` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+        defaultZoom={12}
+        defaultCenter={{ lat: 45.516, lng: -122.679565 }}
+        defaultOptions={{
+          styles: mapStyle,
+          streetViewControl: false,
+          mapTypeControl: false,
+          fullscreenControl: false
+        }}
+      />
+    )
+  }
+}
+
+MyMapComponent.propTypes = {
+  markers: PropTypes.node.isRequired,
+  polylines: PropTypes.node.isRequired,
+  bounds: PropTypes.node.isRequired,
+  mapRef: PropTypes.node.isRequired,
+  buildRoadMap: PropTypes.func.isRequired
+}
 
 export default MyMapComponent
---
+++ client/src/utils/roadMap.js
@@ -0,0 +1,190 @@
+import React from 'react'
+import { Marker, Polyline } from 'react-google-maps'
+
+function randLat() {
+  return Math.floor(Math.random() * 12 + 35)
+}
+function randLong() {
+  return Math.floor(Math.random() * 12 - 100)
+}
+function randEndTime(start) {
+  return Math.floor(Math.random() * 50 + start)
+}
+function randPrio() {
+  return Math.floor(Math.random() * 2.9999)
+}
+
+// Nodes, consists of a timeline and location and priority level
+const nodes = [
+  {
+    start: 0,
+    end: randEndTime(0),
+    location: { lat: randLat(), lng: randLong() },
+    priority: randPrio()
+  },
+  {
+    start: 10,
+    end: randEndTime(10),
+    location: { lat: randLat(), lng: randLong() },
+    priority: randPrio()
+  },
+  {
+    start: 20,
+    end: randEndTime(20),
+    location: { lat: randLat(), lng: randLong() },
+    priority: randPrio()
+  },
+  {
+    start: 30,
+    end: randEndTime(30),
+    location: { lat: randLat(), lng: randLong() },
+    priority: randPrio()
+  },
+  {
+    start: 40,
+    end: randEndTime(40),
+    location: { lat: randLat(), lng: randLong() },
+    priority: randPrio()
+  },
+  {
+    start: 50,
+    end: randEndTime(50),
+    location: { lat: randLat(), lng: randLong() },
+    priority: randPrio()
+  },
+  {
+    start: 60,
+    end: randEndTime(60),
+    location: { lat: randLat(), lng: randLong() },
+    priority: randPrio()
+  },
+  {
+    start: 70,
+    end: randEndTime(70),
+    location: { lat: randLat(), lng: randLong() },
+    priority: randPrio()
+  }
+]
+
+const extendBounds = locations => {
+  let north
+  let east
+  let south
+  let west
+  const bounds = {
+    north,
+    east,
+    south,
+    west
+  }
+
+  const latitudes = locations.map(location => location.lat)
+  const longitudes = locations.map(location => location.lng)
+
+  bounds.north = Math.max(...latitudes)
+  bounds.south = Math.min(...latitudes)
+  bounds.east = Math.max(...longitudes)
+  bounds.west = Math.min(...longitudes)
+
+  return bounds
+}
+
+const buildMap = () => {
+  const markers = nodes.map((node, index) => (
+    <Marker key={index} label={index.toString()} position={node.location} />
+  ))
+
+  const parentNodes = [] // Nodes for main roadmap path
+  const polylines = [] // Polylines, array of objects -> {children, nextParent}
+  const locations = nodes.map(node => node.location) // LatLng objects
+  const bounds = extendBounds(locations)
+
+  parentNodes.push(nodes[0])
+
+  // identify parent nodes
+  if (nodes.length > 1) {
+    for (let index = 0; index < nodes.length; ) {
+      // if (index === nodes.length - 1) {
+      //   break
+      // }
+
+      const prio = nodes[index].priority
+      const children = [] // Polylines to concurrent experiences (nodes)
+      let nextParent // Polyline to next parent
+      const polyline = {
+        children,
+        nextParent
+      }
+
+      // ------------ Search for children ------------------ //
+      let i = index + 1
+      while (
+        i < nodes.length &&
+        nodes[i].priority >= prio &&
+        nodes[i].start < nodes[index].end
+      ) {
+        i += 1
+      }
+
+      // ------------ Build array of polylines ------------- //
+      if (i < nodes.length) {
+        // parentNodes.push(nodes[i])
+        // build polylines
+        let n = index + 1
+        while (n < nodes.length && n < i) {
+          // polylines from parent to children
+          children.push(
+            <Polyline
+              key={n}
+              path={[nodes[index].location, nodes[n].location]}
+              options={{ strokeColor: 'green', strokeOpacity: '0.5' }}
+            />
+          )
+          n += 1
+        }
+        // polyline to next parent
+        nextParent = (
+          <Polyline
+            key={i}
+            path={[nodes[index].location, nodes[i].location]}
+            options={{ strokeColor: 'orange' }}
+          />
+        )
+        polyline.children = children
+        polyline.nextParent = nextParent
+        polylines.push(polyline)
+      } else {
+        // ---------- End of array case ------------- //
+        let n = index + 1
+        while (n < nodes.length && n < i - 1) {
+          // polylines from parent to children
+          children.push(
+            <Polyline
+              key={n}
+              path={[nodes[index].location, nodes[n].location]}
+              options={{ strokeColor: 'green', strokeOpacity: '0.5' }}
+            />
+          )
+          n += 1
+        }
+        // polyline to next parent
+        nextParent = (
+          <Polyline
+            key={i}
+            path={[nodes[index].location, nodes[i - 1].location]}
+            options={{ strokeColor: 'orange' }}
+          />
+        )
+        // Push array of children polylines and nextParent polyline onto polylines array
+        polyline.children = children
+        polyline.nextParent = nextParent
+        polylines.push(polyline)
+      }
+      index = i // Assign index to next parent node index
+    }
+  }
+
+  return { markers, polylines, bounds }
+}
+
+export default buildMap

3aff4c58f693b2576a77a7b590a743c1d558e5a5
Dan Crayne <dan.crayne@gmail.com>
2018-02-26 08:50:20 -0800

    add search class for model common search tasks

---
+++ app/lib/search.rb
@@ -0,0 +1,9 @@
+class Search
+  def self.where_clause_from_fields(fields_to_search)
+    return where_clause = fields_to_search.join(" LIKE :term OR ") + " LIKE :term "
+  end
+
+  def self.term_to_regex(term)
+    return "%#{term}%"
+  end
+end

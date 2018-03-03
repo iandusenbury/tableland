7b74f4221ffa3cb034314bb31e7f4895ad096b7f
Matt <matthewballeza@gmail.com>
2018-03-02 16:22:16 -0800

    [#80] Removed margin around body. Adjusted relevant pages.

--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Map from '../../containers/map'
 
 const Home = () => (
-  <div style={{ height: 'calc(100vh - 72px)' }}>
+  <div style={{ height: 'calc(100vh - 56px)' }}>
     <Map />
   </div>
 )

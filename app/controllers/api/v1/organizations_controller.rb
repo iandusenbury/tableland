5d185974f8c6d8ee740161ef6bf12251a58bd914
Evan White <evwhite2993@gmail.com>
2018-03-08 19:05:32 -0800

    [#137] Add validation and permission checks to organization update - The current strategy for adding a url to an organization is very lenient for now, this can be changed if we want.

--- app/controllers/api/v1/organizations_controller.rb
+++ app/controllers/api/v1/organizations_controller.rb
@@ -2,6 +2,7 @@ module Api::V1
   class OrganizationsController < ApiBaseController
     before_action :set_organization, only: [:show, :update, :grant_permission, :admins]
     before_action :allow_if_visible, except: [:index, :show] 
+    before_action :validate_update_params, only: :update
 
     # GET /v1/organizations
     def index
@@ -26,11 +27,8 @@ module Api::V1
 
     # PATCH/PUT /v1/organizations/{id}
     def update
-      if @organization.update(update_organization_params)
-        render json: @organization, include: '', status: :ok
-      else
-        render json: @organization.errors, status: :unprocessable_entity
-      end
+      @organization.update!(update_organization_params)
+      render json: @organization, include: '', status: :ok
     end
 
     # POST /v1/organizations/{id}/permissions
@@ -65,6 +63,18 @@ module Api::V1
         raise ExceptionTypes::UnauthorizedError.new("Your account has been blocked") unless (current_user.visible? || current_user.super_admin?)
       end
 
+      # Validate the request payload and permissions when updating an existing organization
+      def validate_update_params
+        if current_user.user?
+          raise ExceptionTypes::UnauthorizedError.new("Admin access only")
+        end
+        if current_user.admin?
+          raise ExceptionTypes::UnauthorizedError.new("You do not have permission to edit the organization with ID #{@organization.id}") unless @organization.admins.exists? current_user.id
+        end
+        validate_url
+        validate_visible
+      end
+
       # Attempt to prevent the creation of the same organization with slightly different names
       # by trying to find an existing organization with almost the exact same name and either
       # an exact match for the first two address lines or an exact match for the geographic coordinates.
@@ -80,6 +90,27 @@ module Api::V1
         end
 
         return found_duplicate || valid_new_organization
+      end
+
+      # Validate that the url attribute in the payload does not contain invalid characters or spaces
+      # This parse method is very lenient and could be made more strict if we want
+      def validate_url
+        url = params[:url] || params[:organization][:url]
+        begin
+          URI.parse(url) if url
+        rescue URI::InvalidURIError
+          raise ExceptionTypes::BadRequestError.new("Invalid characters used in URL: #{url}")
+        end
+      end
+
+      # Validate that the visible attribute in the payload is strictly
+      # boolean true or false
+      def validate_visible
+        visible = params[:visible] || params[:organization][:visible]
+
+        if visible && visible.to_s != "true" && visible.to_s != "false"
+          raise ExceptionTypes::BadRequestError.new("Invalid format for visible: #{visible}, must be boolean true or false")
+        end
       end
 
       # Permit only the appropriate parameters for the creation of a new organization

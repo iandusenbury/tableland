0395504be76e99eb82683b017d7f36617cf97e3b
djarredon <djarredon@gmail.com>
2018-03-09 16:54:47 -0800

    Started constructing About page.

--- client/src/components/about/index.js
+++ client/src/components/about/index.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import './style.css'
 
 export default () => (
-  <div>
-    <h1>About Us</h1>
-    <p>Hello Medium!</p>
+  <div className="about-main-div">
+    <h1 className="about-header-one">About MESA</h1>
+    <p className="about-text">
+      <b>M</b>athematics<br />
+      <b>E</b>ngineering<br />
+      <b>S</b>cience<br />
+      <b>A</b>chievement<br />
+    </p>
+
+    <p className="about-text">
+      <a href='http://oregonmesa.org/'>MESA Homepage</a>
+    </p>
+
+    <h2 className="about-header-one">About This App</h2>
+    <p className="about-text">
+      MESA Roadmaps lets users plot out their career and view their career history on a Google Maps interface. The app is designed for students interested in STEM to see just how professionals in the field got to where they are. 
+    </p>
+
+    <h2 className="about-header-one">About Us</h2>
+    <p className="about-text">
+    </p>
   </div>
 )

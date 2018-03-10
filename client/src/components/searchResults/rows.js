bc0c48062ca2095921289ad57b0fc3e88d480366
Matt <matthewballeza@gmail.com>
2018-03-09 17:47:14 -0800

    [#60] Moved media reducer function to Search Reducer.

--- client/src/components/searchResults/rows.js
+++ client/src/components/searchResults/rows.js
@@ -1,31 +1,8 @@
 import React from 'react'
-import { find, propEq } from 'ramda'
 import { TableRow, TableRowColumn, Avatar, FlatButton } from 'material-ui'
-import BusinessIcon from 'material-ui/svg-icons/communication/business'
-import GroupIcon from 'material-ui/svg-icons/social/group'
-
-// Default Avatar images
-const userImage = require('../../assets/images/portrait.png')
-
-const orgIcon = <BusinessIcon />
-const progIcon = <GroupIcon />
-
-const getDefaultImage = type => {
-  if (type === 'Program') return progIcon
-  if (type === 'Organization') return orgIcon
-
-  return userImage
-}
-
-// Returns image url belonging to profile or default url
-const retrieveMedia = (media, type) => {
-  const image = find(propEq('category', 'image'))(media)
-  return image && image.url ? image.url : getDefaultImage(type)
-}
 
 // Returns an Avatar component with either a photo or icon
-const getAvatar = (media, type) => {
-  const url = retrieveMedia(media, type)
+const getAvatar = url => {
   const avatar =
     typeof url === 'string' ? (
       <Avatar size={60} src={url} />
@@ -58,9 +35,9 @@ const tableRow = profile => {
 }
 
 export const renderProgram = program => {
-  const { id, type, name, description, media, url: contactUrl } = program
+  const { id, type, name, description, imageUrl, url: contactUrl } = program
 
-  const avatar = getAvatar(media, 'Program')
+  const avatar = getAvatar(imageUrl)
   const profileName = <h2>{name}</h2>
   const info = (
     <ul>
@@ -89,7 +66,7 @@ export const renderOrganization = organization => {
     addressLine3,
     city,
     state,
-    media,
+    imageUrl,
     url: contactUrl
   } = organization
 
@@ -115,7 +92,7 @@ export const renderOrganization = organization => {
     address.push(extendedAddress)
   }
 
-  const avatar = getAvatar(media, 'Organization')
+  const avatar = getAvatar(imageUrl)
   const profileName = <h2>{name}</h2>
   const info = (
     <ul>{address.map((line, index) => <li key={index}>{line}</li>)}</ul> // eslint-disable-line
@@ -140,11 +117,11 @@ export const renderUser = user => {
     lastName,
     mainTitle,
     mainLocation,
-    media,
+    imageUrl,
     contactUrl
   } = user
 
-  const avatar = getAvatar(media, 'User')
+  const avatar = getAvatar(imageUrl)
   const profileName = (
     <h2>
       {firstName} {lastName}

df604912db3020f35a63c02ddbdf2f53091c4c75
Matt <matthewballeza@gmail.com>
2018-03-23 17:10:11 -0700

    [#142] Small fixes for search results page.

--- client/src/components/searchResults/rows.js
+++ client/src/components/searchResults/rows.js
@@ -25,32 +25,32 @@ const tableRow = profile => {
         <div className="search-table-icon search-table-border">
           <div className="search-table-avatar">{avatar}</div>
           {type === 'User' &&
-            <FlatButton 
-              className="search-table-flatbutton" 
+            <FlatButton
+              className="search-table-flatbutton"
               hoverColor="#e7e0d7"
-              containerElement={<Link to={`/roadmap/${id}`} />} 
-            >   
+              containerElement={<Link to={`/roadmap/${id}`} />}
+            >
               View profile
               </FlatButton>
-          }   
+          }
           {type === 'Organization' &&
-            <FlatButton 
-              className="search-table-flatbutton" 
+            <FlatButton
+              className="search-table-flatbutton"
               hoverColor="#e7e0d7"
-              containerElement={<Link to={`/organization/${id}`} />} 
-            >   
+              containerElement={<Link to={`/organization/${id}`} />}
+            >
               View profile
               </FlatButton>
-          }   
+          }
           {type === 'Program' &&
-            <FlatButton 
-              className="search-table-flatbutton" 
+            <FlatButton
+              className="search-table-flatbutton"
               hoverColor="#e7e0d7"
-              containerElement={<Link to={`/program/${id}`} />} 
-            >   
+              containerElement={<Link to={`/program/${id}`} />}
+            >
               View profile
               </FlatButton>
-          }   
+          }
         </div>
         <div className="search-table-about">{info}</div>
         <div className="search-table-contact">{contact}</div>
@@ -61,14 +61,12 @@ const tableRow = profile => {
 
 
 export const renderProgram = program => {
-  const { id, type, name, description, imageUrl, url: contactUrl } = program
+  const { id, type, name, parentOrganizationNames, imageUrl, url: contactUrl } = program
 
   const avatar = getAvatar(imageUrl)
   const profileName = <h2>{name}</h2>
   const info = (
-    <ul>
-      <li>{description}</li>
-    </ul>
+    <p>Hosted at: {parentOrganizationNames[0] && parentOrganizationNames[0][0]}</p>
   )
   //const contact = <a href={contactUrl}>{contactUrl}</a>
   const contact = <RaisedButton linkButton={true} href={contactUrl} label='View Website' />
